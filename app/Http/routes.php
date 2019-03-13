16411f078a9972350e4f6f8fae54b6f3f5cc48c1
gmastrbit <gmastrbit@gmail.com>
2019-03-13 23:35:18 +0300

    add: routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,59 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// завдяки групі ми визначимо посередників, які будуть використовуватися певних маршрутів
+Route::group(['middleware' => 'web'], function (){
+
+    // маршрут головної сторінки
+    Route::match(['get', 'post'], '/', ['uses' => 'IndexController@execute', 'as' => 'home']);
+    Route::get('/page/{alias}', ['uses' => 'PageController@execute', 'as' => 'page']);
+
+    // для аутентифікації користувачів
+    Route::auth();
 });
+
+// адмін-частина
+Route::group(['prefix' => 'admin', 'middleware' => 'auth', function(){
+
+    // головна сторінка адмінки
+    Route::get('/', function(){
+
+    });
+
+    // admin/pages
+    Route::group(['prefix' => 'pages'], function(){
+
+        // маршрут головної сторінки по керування сторінками
+        Route::get('/', ['uses' => 'PagesController@execute', 'as' => 'pages']);
+
+        // admin/pages/add
+        Route::match(['get', 'post'], '/add', ['uses' => 'PageAddController@execute', 'as' => 'pagesAdd']);
+
+        // admin/edit/2
+        // uses => PagesEditController@execute - який метод обробить поточний маршрут
+        Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses' => 'PagesEditController@execute', 'as' => 'pagesEdit']);
+    });
+
+    // admin/portfolio
+    Route::group(['prefix' => 'portfolios'], function(){
+
+        // маршрут головної сторінки портфоліо
+        Route::get('/', ['uses' => 'PortfolioController@execute', 'as' => 'portfolio']);
+
+        Route::match(['get', 'post'], '/add', ['uses' => 'PortfolioAddController@execute', 'as' => 'portfolioAdd']);
+
+        Route::match(['get', 'post', 'delete'], '/edit/{portfolio}', ['uses' => 'PortfolioEditController@execute', 'as' => 'portfolioEdit']);
+    });
+
+    // admin/services
+    Route::group(['prefix' => 'services'], function(){
+
+        // маршрут головної сторінки портфоліо
+        Route::get('/', ['uses' => 'ServiceController@execute', 'as' => 'services']);
+
+        Route::match(['get', 'post'], '/add', ['uses' => 'ServiceAddController@execute', 'as' => 'serviceAdd']);
+
+        Route::match(['get', 'post', 'delete'], '/edit/{service}', ['uses' => 'ServiceEditController@execute', 'as' => 'serviceEdit']);
+    });
+
+}]);
