dd44da544ebd8de0ad0d0958f20ff7a1d4d83e4f
gmastrbit <gmastrbit@gmail.com>
2019-03-15 15:12:09 +0300

    add: models, views

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,12 +6,26 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Page;
+use App\People;
+use App\Service;
+use App\Portfolio;
+
 class IndexController extends Controller
 {
     //
     public function execute(Request $request)
     {
+        $pages = Page::all();
+        $portfolios = Portfolio::get(['name', 'filter', 'images']);
+//        $portfolios = Portfolio::all();
+        $services = Service::where('id', '<', '20');
+//        $services = Service::all();
+        $peoples = People::take(3)->get();
+//        $peoples = People::all();
 
-        return view('layouts.site');
+//        dd($peoples);
+
+        return view('site.index');
     }
 }
